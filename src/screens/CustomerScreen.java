7f301cee0db001f8268bdecb5b40a651f5d61430
RyamAlmalki <ryam.malki@gmail.com>
2023-02-24 10:02:27 +0300

    setup

---
+++ src/screens/CustomerScreen.java
@@ -0,0 +1,11 @@
+package screens;
+
+import javax.swing.*;
+
+public class CustomerScreen extends JFrame {
+
+    public CustomerScreen(){
+        this.setSize(500,500);
+        this.setVisible(true);
+    }
+}
