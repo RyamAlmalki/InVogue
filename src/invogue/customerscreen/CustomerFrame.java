a01b9add03c0417b365e7c6f03398a235eda369b
RyamAlmalki <ryam.malki@gmail.com>
2023-02-24 09:47:50 +0300

    setup

---
+++ src/invogue/customerscreen/CustomerFrame.java
@@ -0,0 +1,13 @@
+package com.invogue.customerscreen;
+
+import javax.swing.JFrame;
+
+
+public class CustomerFrame extends JFrame{
+
+    public CustomerFrame(){
+        this.setSize(500,500);
+        this.setVisible(true);
+    }
+
+}
